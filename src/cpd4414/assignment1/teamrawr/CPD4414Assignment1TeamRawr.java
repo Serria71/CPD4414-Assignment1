9202aa571685e25b731e4c5b2bda4c608b07d0a9
c0640858 <c0640858@SB-N208-34.lambton.on.ca>
2016-01-14 14:21:17 -0500

    Created stringPower skeleton.

--- src/cpd4414/assignment1/teamrawr/CPD4414Assignment1TeamRawr.java
+++ src/cpd4414/assignment1/teamrawr/CPD4414Assignment1TeamRawr.java
@@ -17,5 +17,8 @@ public class CPD4414Assignment1TeamRawr {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    public static String stringPower(String name, int num) { 
+       return name;
+    }
+       
 }
