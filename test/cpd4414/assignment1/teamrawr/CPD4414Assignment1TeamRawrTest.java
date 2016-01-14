a44b515da94de985a64fdeec45a025ecd5f5b853
'T.J. <c0640785@mylambton.ca>
2016-01-14 14:40:46 -0500

    Created testEmptyStringShouldReturnEmptyString method.

---
+++ test/cpd4414/assignment1/teamrawr/CPD4414Assignment1TeamRawrTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpd4414.assignment1.teamrawr;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author c0640785
+ */
+public class CPD4414Assignment1TeamRawrTest {
+    
+    public CPD4414Assignment1TeamRawrTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class CPD4414Assignment1TeamRawr.
+     */
+
+    /**
+     * Test of stringPower method, of class CPD4414Assignment1TeamRawr.
+     */
+    @Test
+    public void testEmptyStringShouldReturnEmptyString() {
+        System.out.println("stringPower");
+        String name = "";
+        int num = 0;
+        String expResult = "";
+        String result = CPD4414Assignment1TeamRawr.stringPower(name, num);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+    
+}
